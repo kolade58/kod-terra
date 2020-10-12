5172c95e8e2c19f2c76f0b859e9dbeb3834e3432
kolade dairo <koladeaisha@gmail.com>
2020-10-12 03:35:54 -0400

    add main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,11 @@ resource "aws_internet_gateway" "gw" {
     Name = "terra-igw"
   }
 }
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.10.0.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
