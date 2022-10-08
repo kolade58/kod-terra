79ef9f8b3174ebbeb825737920c23c3975776a4e
kolade58 <koladedairo5050@gmail.comm>
2022-10-08 04:46:54 -0400

    removingrt_private

--- main.tf
+++ main.tf
@@ -29,8 +29,3 @@ resource "aws_route_table_association" "rt_public1" {
     subnet_id = aws_subnet.public_main_us_east_1a.id
     route_table_id = aws_route_table.vpc.main_public.id
 }
-
-resource "aws_route_table_association" "rt_public2" {
-    subnet_id = aws_subnet.public_main_us_east_1b.id
-    route_table_id = aws_route_table.vpc.main_public.id
-  }
